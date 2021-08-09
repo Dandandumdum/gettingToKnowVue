8b591e487657626d85baa0cecade6f7ad5b6ba19
danielDumville <daniel.dumville@se.experis.com>
2021-08-09 14:00:36 +0200

    Added codeDemo code

--- knowingvue/src/App.vue
+++ knowingvue/src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Bands</h3>
+
+    <ul>
+      <li v-for="band of bands" :key="band" :class="{ 'favouriteBand': band === favouriteBand}">{{band}} <button @click="addBand(band)">Add</button></li>
+    </ul>
+      
+      <div v-if="favouriteBands.length > 0">
+      <h3>Favourite Bands</h3>
+
+    <ul>
+      <li v-for="band of favouriteBands" :key="band">{{band}} <button @click="removeBand(band)">Remove</button></li>
+    </ul>
+
+    <input type="text" name="favouriteBand" id="favouriteBand" v-model="favouriteBand">
+      </div>
+      <div v-else>
+        <h3>No favourites yet...</h3>
+
+      </div>
+    
+    
   </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      bands: [
+        "Megmeth",
+        "NXSX",
+        "John's band"
+      ],
+      favouriteBands:[
+      ],
+      favouriteBand: ''
+    }
+  },
+  methods: {
+    addBand(band){
+      if(!this.favouriteBands.includes(band)){
+          this.favouriteBands.push(band)
+      }
+      
+    },
+    removeBand(band){
+        this.favouriteBands.pop(band)
+       
+    }
   }
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .favouriteBand {
+    color: blueviolet;
+  }
 </style>
